4397dccd3a7014e6167361dc4815e4195faaaff4
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-06-06 22:19:03 +0300

    06/06 - Add navigation's graph.

---
+++ app/src/main/java/com/tomaslab/app/presenter/FragmentContentWeapons.kt
@@ -0,0 +1,7 @@
+package com.tomaslab.app.presenter
+
+import androidx.fragment.app.Fragment
+import com.tomaslab.app.R
+
+class FragmentContentWeapons: Fragment(R.layout.fragment_content_weapons) {
+}
---
+++ app/src/main/java/com/tomaslab/app/presenter/FragmentSelectTypeWeapons.kt
@@ -0,0 +1,7 @@
+package com.tomaslab.app.presenter
+
+import androidx.fragment.app.Fragment
+import com.tomaslab.app.R
+
+class FragmentSelectTypeWeapons: Fragment(R.layout.fragment_select_type_weapons) {
+}
