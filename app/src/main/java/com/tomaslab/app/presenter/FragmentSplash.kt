6a2143d957c4869734fa055e1e6acb79cc02069f
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-06-08 22:53:09 +0300

    08/06 - Splash screen done, but need refractory theme.xml

--- app/src/main/java/com/tomaslab/app/presenter/FragmentSplash.kt
+++ app/src/main/java/com/tomaslab/app/presenter/FragmentSplash.kt
@@ -1,7 +1,27 @@
 package com.tomaslab.app.presenter
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.tomaslab.app.R
+import com.tomaslab.app.databinding.FragmentSplashBinding
 
 class FragmentSplash: Fragment(R.layout.fragment_splash) {
+
+    private lateinit var binding:FragmentSplashBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSplashBinding.bind(view)
+
+        binding.fragmentSplashScreen.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentSplash_to_fragmentMainMenu)
+        }
+
+    }
+
+
+
+
 }
