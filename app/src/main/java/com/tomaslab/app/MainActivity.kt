a2f00ab5c1f118daa355ab602e4b1f39544d2548
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-06-09 20:24:22 +0300

    09/06 - gone screen bar. Full screen.

--- app/src/main/java/com/tomaslab/app/MainActivity.kt
+++ app/src/main/java/com/tomaslab/app/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.tomaslab.app
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.tomaslab.app.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        viewGone ()
         setContentView(binding.root)
+    }
+
+
+    // Full screen
+    private fun viewGone(){
+        val decorView: View = window.decorView
+        val uiOptions: Int = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        decorView.systemUiVisibility = uiOptions
     }
 }
