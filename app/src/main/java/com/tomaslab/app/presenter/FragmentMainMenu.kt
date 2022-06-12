6cffac269ea2aef821c6198501e8f57a22e80305
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-06-12 12:03:59 +0300

    12/06 - Local eng. Job with FragmentMainMenu.kt and need random text in FragmentSplash.kt

--- app/src/main/java/com/tomaslab/app/presenter/FragmentMainMenu.kt
+++ app/src/main/java/com/tomaslab/app/presenter/FragmentMainMenu.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import com.tomaslab.app.R
 import com.tomaslab.app.databinding.FragmentMainMenuLandBinding
 
-class FragmentMainMenu: Fragment(R.layout.fragment_main_menu_land) {
+class FragmentMainMenu: Fragment(R.layout.fragment_main_menu_land), View.OnClickListener {
 
     private  var binding: FragmentMainMenuLandBinding? = null
 
@@ -14,13 +14,27 @@ class FragmentMainMenu: Fragment(R.layout.fragment_main_menu_land) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainMenuLandBinding.bind(view)
 
-
-
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0){
+            binding?.im1 -> selectLand(1)
+            binding?.im2 -> selectLand(2)
+            binding?.im3 -> selectLand(3)
+            binding?.im4 -> selectLand(4)
+            binding?.im5 -> selectLand(5)
+            binding?.im6 -> selectLand(6)
+            binding?.im7 -> selectLand(7)
+            binding?.im8 -> selectLand(8)
+        }
+    }
+
+    private fun selectLand(id: Int){
+
     }
 }
