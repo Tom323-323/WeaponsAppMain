1501085f53978c1bda16a2758860a64556661886
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-06-17 20:49:39 +0300

    14/06 - Create RecyclerView. Part 3.

--- app/src/main/java/com/tomaslab/app/domain/AdapterRV.kt
+++
@@ -1,2 +0,0 @@
-package com.tomaslab.app.domain
-
--- app/src/main/java/com/tomaslab/app/presenter/FragmentSelectWeapons.kt
+++ app/src/main/java/com/tomaslab/app/presenter/FragmentSelectWeapons.kt
@@ -6,12 +6,14 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tomaslab.app.R
 import com.tomaslab.app.databinding.FragmentSelectWeaponsLandBinding
+import com.tomaslab.app.domain.AdapterFragmentSelectWeapons
 import com.tomaslab.app.presenter.model.WeaponsModel
 
 
 class FragmentSelectWeapons: Fragment(R.layout.fragment_select_weapons_land) {
 
     private var binding: FragmentSelectWeaponsLandBinding? = null
+    val dataWeapons = arrayListOf<WeaponsModel>()
 
     companion object {
         const val ID_LAND = "id_land"
@@ -21,14 +23,14 @@ class FragmentSelectWeapons: Fragment(R.layout.fragment_select_weapons_land) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSelectWeaponsLandBinding.bind(view)
 
-        val id_land = requireArguments().getInt(ID_LAND)
+        val id_land = requireArguments().getInt(ID_LAND) // Get argument from FragmentMainMenu
         landManager(id_land)
 
         // ReccyclerView___________________________________________________
         val rv = binding!!.rvTypeWeapons
         rv.layoutManager = LinearLayoutManager(requireContext())
         rv.setHasFixedSize(true)
-        val dataWeapons = arrayListOf<WeaponsModel>()
+        rv.adapter = AdapterFragmentSelectWeapons(dataWeapons) //Need crate ADAPTER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         //_________________________________________________________________
 
@@ -48,6 +50,10 @@ class FragmentSelectWeapons: Fragment(R.layout.fragment_select_weapons_land) {
             binding?.titleLand?.text = title_array_land[id]} // Set title text in Head Line
 
         binding?.imgLand?.setImageResource(img_array_land[id]) // Set image in Head Line
+
+
+        // RecyclerView add data________________________________________________
+
 
     }
 
